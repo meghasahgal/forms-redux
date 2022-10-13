32cf004560fa9b1846f92e20c82a1c2ec132f9d5
Megha Sahgal <meghas@gmail.com>
2022-10-13 23:41:03 +0000

    finished!

--- src/components/ArticleInput/index.js
+++ src/components/ArticleInput/index.js
@@ -1,49 +1,58 @@
-import { useState } from 'react';
-import { nanoid } from 'nanoid';
-import './ArticleInput.css';
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { nanoid } from "nanoid";
+import "./ArticleInput.css";
+import { addArticle } from "../../store/articleReducer";
 
 const ArticleInput = () => {
-  const [title, setTitle] = useState('');
-  const [body, setBody] = useState('');
-  const [imageUrl, setImageUrl] = useState('');
+  //add dispatch
+  const dispatch = useDispatch();
+
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    //get data here
+    const articleData = { id: nanoid(), title, body, imageUrl };
+    //dispatch the data with the action creator
+    dispatch(addArticle(articleData));
     reset();
   };
 
   const reset = () => {
-    setTitle('');
-    setImageUrl('');
-    setBody('');
+    setTitle("");
+    setImageUrl("");
+    setBody("");
   };
 
   return (
-    <div className='inputBox'>
+    <div className="inputBox">
       <h1>Create Article</h1>
       <form onSubmit={handleSubmit}>
         <input
-          type='text'
+          type="text"
           onChange={(e) => setTitle(e.target.value)}
           value={title}
-          placeholder='Title'
-          name='title'
+          placeholder="Title"
+          name="title"
         />
         <input
-          type='text'
+          type="text"
           onChange={(e) => setImageUrl(e.target.value)}
           value={imageUrl}
-          placeholder='Image URL'
-          name='imageUrl'
+          placeholder="Image URL"
+          name="imageUrl"
         />
         <textarea
           value={body}
           onChange={(e) => setBody(e.target.value)}
-          name='body'
-          placeholder='Add your entry'
-          rows='10'
+          name="body"
+          placeholder="Add your entry"
+          rows="10"
         ></textarea>
-        <button type='submit'>Submit</button>
+        <button type="submit">Submit</button>
       </form>
     </div>
   );
